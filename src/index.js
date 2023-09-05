b8e30e44f89cec5d8e78c1395e9fb035ba0126d2
Nathalia285 <nathalia285@gmail.com>
2023-09-04 23:20:15 -0500

    Prueba

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import './helpers/chat&emojis.js'
 import './styles/css/template.css'
-import '../node_modules/sweetalert2/dist/sweetalert2.min.css'
-import Swal from '../node_modules/sweetalert2/src/sweetalert2.js'
-
-// Swal.fire({
-//     title: 'Nice!',
-//     text: 'We are on template page!',
-//     icon: 'success',
-//     confirmButtonText: 'Cool'
-// })
+/* import '../node_modules/sweetalert2/dist/sweetalert2.min.css'
+import Swal from '../node_modules/sweetalert2/src/sweetalert2.js' */
 
